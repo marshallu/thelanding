82119fb64a152111fc739e645c705c811d6d6f1e
Christopher McComas <cmccomas@marshall.edu>
2020-11-02 12:52:28 -0500

    wip

--- blocks/block-contact.php
+++ blocks/block-contact.php
@@ -13,6 +13,7 @@ $left_column  = get_field( 'left_column' );
 $right_column = get_field( 'right_column' );
 
 echo '<div id="contact" x-ref="contact" class="flex flex-wrap">';
+
 echo '<div class="w-full lg:w-1/2 bg-gray-light pb-12">';
 echo '<div class="text-white uppercase text-lg text-center bg-green py-4 font-semibold">Follow Us</div>';
 echo '<div class="arrow-top green-arrow-top"></div>';
@@ -24,8 +25,9 @@ echo '</div>';
 echo '<div class="w-full lg:w-1/2 bg-blue-light pb-12">';
 echo '<div class="text-white uppercase text-lg text-center bg-teal py-4 font-semibold">Contact Us</div>';
 echo '<div class="arrow-top teal-arrow-top"></div>';
-echo '</div>';
 echo '<div class="py-6">';
 echo $right_column;
 echo '</div>';
+echo '</div>';
+
 echo '</div>';
